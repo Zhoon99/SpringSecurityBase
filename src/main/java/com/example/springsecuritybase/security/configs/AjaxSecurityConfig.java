a514bcf31e10cc39865850c87e20ac5f2d71c8bf
Zhoon99 <chjh827@gmail.com>
2022-07-15 16:35:00 +0900

    Ajax Custom DSLs 구현

--- src/main/java/com/example/springsecuritybase/security/configs/AjaxSecurityConfig.java
+++ src/main/java/com/example/springsecuritybase/security/configs/AjaxSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.example.springsecuritybase.security.configs;
 
+import com.example.springsecuritybase.security.common.AjaxLoginAuthenticationEntryPoint;
 import com.example.springsecuritybase.security.filter.AjaxLoginProcessingFilter;
+import com.example.springsecuritybase.security.handler.AjaxAccessDeniedHandler;
 import com.example.springsecuritybase.security.handler.AjaxAuthenticationFailureHandler;
 import com.example.springsecuritybase.security.handler.AjaxAuthenticationSuccessHandler;
 import com.example.springsecuritybase.security.provider.AjaxAuthenticationProvider;
@@ -13,10 +15,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
 @Order(0) //설정 클래스가 여러개일 경우 순서를 지정해 줘야 함
 public class AjaxSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -50,21 +52,40 @@ public class AjaxSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .antMatcher("/api/**") //api 로 시작하는 요청에 한해서만 동작
                 .authorizeRequests()
-                .anyRequest().authenticated()
+                .antMatchers("/api/messages").hasRole("MANAGER")
+                .anyRequest().authenticated();
+//                .and()
+//                .addFilterBefore(ajaxLoginProcessingFilter(), UsernamePasswordAuthenticationFilter.class);
 
-        .and()
-                .addFilterBefore(ajaxLoginProcessingFilter(), UsernamePasswordAuthenticationFilter.class)
-        ;
+        http
+                .exceptionHandling()
+                .authenticationEntryPoint(new AjaxLoginAuthenticationEntryPoint())
+                .accessDeniedHandler(ajaxAccessDeniedHandler());
 
         http.csrf().disable(); //csrf 기능 끄기
+
+        customConfigurerAjax(http);
     }
 
-    @Bean
-    public AjaxLoginProcessingFilter ajaxLoginProcessingFilter() throws Exception {
-        AjaxLoginProcessingFilter ajaxLoginProcessingFilter = new AjaxLoginProcessingFilter();
-        ajaxLoginProcessingFilter.setAuthenticationManager(authenticationManagerBean());
-        ajaxLoginProcessingFilter.setAuthenticationSuccessHandler(ajaxAuthenticationSuccessHandler());
-        ajaxLoginProcessingFilter.setAuthenticationFailureHandler(ajaxAuthenticationFailureHandler());
-        return  ajaxLoginProcessingFilter;
+    private void customConfigurerAjax(HttpSecurity http) throws Exception {
+        http
+                .apply(new AjaxLoginConfigurer<>())
+                .successHandlerAjax(ajaxAuthenticationSuccessHandler())
+                .failureHandlerAjax(ajaxAuthenticationFailureHandler())
+                .setAuthenticationManager(authenticationManagerBean())
+                .loginProcessingUrl("/api/login");
     }
+
+    public AccessDeniedHandler ajaxAccessDeniedHandler() {
+        return new AjaxAccessDeniedHandler();
+    }
+
+//    @Bean
+//    public AjaxLoginProcessingFilter ajaxLoginProcessingFilter() throws Exception {
+//        AjaxLoginProcessingFilter ajaxLoginProcessingFilter = new AjaxLoginProcessingFilter();
+//        ajaxLoginProcessingFilter.setAuthenticationManager(authenticationManagerBean());
+//        ajaxLoginProcessingFilter.setAuthenticationSuccessHandler(ajaxAuthenticationSuccessHandler());
+//        ajaxLoginProcessingFilter.setAuthenticationFailureHandler(ajaxAuthenticationFailureHandler());
+//        return  ajaxLoginProcessingFilter;
+//    }
 }
